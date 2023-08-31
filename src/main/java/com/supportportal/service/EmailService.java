8b8d44f9e4a0bf392944c81fd2a755f1a67ecafe
namnam2303 <imsofh@gmail.com>
2023-08-31 08:10:17 +0700

    The first commit

---
+++ src/main/java/com/supportportal/service/EmailService.java
@@ -0,0 +1,44 @@
+package com.supportportal.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMail( String to,  String subject, String body) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false);
+
+            mimeMessageHelper.setFrom(fromEmail);
+            mimeMessageHelper.setTo(to);
+            //mimeMessageHelper.setCc(cc);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(body);
+
+//            for (int i = 0; i < file.length; i++) {
+//                mimeMessageHelper.addAttachment(
+//                        file[i].getOriginalFilename(),
+//                        new ByteArrayResource(file[i].getBytes()));
+//            }
+
+            javaMailSender.send(mimeMessage);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
